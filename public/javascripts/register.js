3c7b594b7e4e897f985f03f52c1189dccfa97cfa
UAlyu18 <lyu18@email.arizona.edu>
2020-11-13 13:21:50 -0500

    register_info_validation

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -3,13 +3,33 @@ function sendRegisterRequest() {
     let password = $('#password').val();
     let fullName = $('#fullName').val();
     let passwordConfirm = $('#passwordConfirm').val();
-    
+    let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    let pwdReg =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+
     // Check to make sure the passwords match
     // FIXME: Check to ensure strong password 
+    if (!emailReg.test(email)){
+
+      $('#failMessage1').text("Invalid email address!");
+      $("#failMessage1").css({color: "red"});
+      $('#failMessage1').show();
+    }
+    if (!pwdReg.test(password)){
+      $('#failMessage2').html("<span>Password is not strong enough!"
+                                +"<ol><li>At least one capital letter</li>"
+                                +"<li>At least one lower case letter</li>"
+                                +"<li>At least one number</li>"
+                                +"<li>At least one lower special character</li>"
+                                +"</ol>");
+      $('#failMessage2').css({color: "red"});
+      $("#failMessage2").show();
+      
+    }
     if (password != passwordConfirm) {
-      $('#ServerResponse').html("<span class='red-text text-darken-2'>Passwords do not match.</span>");
-      $('#ServerResponse').show();
-      return;
+      $('#failMessage3').html("<span>Passwords do not match.</span>");
+      $('#failMessage3').css({color: "red"});
+      $('#failMessage3').show();
     }
     
     $.ajax({
