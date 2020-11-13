c891430d86c6fd05c6c16ed6dc1c8f48f726bb85
9NdL <9ndl@9NdLs-MacBook.local>
2020-11-13 11:43:38 -0700

    fix focus issues in the registration page

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,9 @@
 //users routes
+/*const express = require('express');
+let router = express.Router();
+let bcrypt = require("bcryptjs");
+let jwt = require("jwt-simple");
+let fs = require('fs');
+let User = require('../models/users');
+let Device = require('../models/device');
+*/
