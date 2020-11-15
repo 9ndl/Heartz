f7c99460d8d1d581617e98726f8d01a00e8bb50d
9ndl <73861931+9ndl@users.noreply.github.com>
2020-11-14 17:20:56 -0700

    Update BPMMonitorSM.h

--- IoTcode/BPMMonitorSM.h
+++ IoTcode/BPMMonitorSM.h
@@ -1,40 +1,31 @@
 //-------------------------------------------------------------------
-
 #ifndef BPMMonitorSM_H
 #define BPMMonitorSM_H
-
 //-------------------------------------------------------------------
-
 #include <vector>
 #include <Wire.h>
 #include <time.h>
 #include "MAX30105.h"
-
 //-------------------------------------------------------------------
-
 using namespace std;
-
 //-------------------------------------------------------------------
-
 class BPMMonitorSM {
-   enum State { S_Init, S_ReadSensor, S_Report};
-
-private:
-   State state;
-   long lastBeat;
-   int tick;
-   float beatsPerMinute;
-   MAX30105& heartSensor;
-   vector<float> bpmHistory;
-   bool sampleReported;
-    
-public:
-   BPMMonitorSM(MAX30105& mySensor);  
-   void execute();
-   float getBPM();
+    enum State { S_Init, S_ReadSensor, S_Report, S_Reminder};
+    private:
+        State state;
+        long lastBeat;
+        int tick;
+        int led;
+        int tick2;
+        int flag;
+        float beatsPerMinute;
+        MAX30105& heartSensor;
+        vector<float> bpmHistory;
+        bool sampleReported;
+    public:
+        BPMMonitorSM(MAX30105& mySensor);
+        void execute();
+        float getBPM();
 };
-
 //-------------------------------------------------------------------
-
 #endif
-
