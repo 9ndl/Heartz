fe99eef89b505f262fbe7b65f43800d2f9e024ac
9NdL <9ndl@9NdLs-MacBook.local>
2020-12-05 14:08:24 -0700

    fix user route

--- routes/users.js
+++ routes/users.js
@@ -92,10 +92,10 @@ router.post('/signin', function(req, res) {
         let decodedToken = jwt.decode(authToken, secret);
         //find the decoded email in the db
         User.findOne({email: decodedToken.email}, function(err, user) {
-            if (err) {
-                res.status(400).json({ success: false, message: "Error contacting DB. Please contact support."});
-            }
-            else {
+          if (err) {
+            res.status(400).json({ success: false, message: "Error contacting DB. Please contact support."});
+          }
+          else{
                 //if found add info to the object 
                 accountInfo["success"] = true;
                 accountInfo["email"] = user.email;
@@ -103,42 +103,21 @@ router.post('/signin', function(req, res) {
                 accountInfo["lastAccess"] = user.lastAccess;
                 accountInfo["devices"] = [];// Array of devices
                 accountInfo["Readings"]= [Reading];
-                accountInfo["OXResults"] = [];
-                accountInfo["timestamps"] = [];
-                //send info back
-                //res.status(200).json(accountInfo);
                 Reading.find({userEmail: decodedToken.email}, function(err, allReadings){
                   if(!err){
-                    //console.log(allReadings[0].userEmail);
                     accountInfo.Readings = allReadings;
-                    //console.log(accountInfo.Readings[0].userEmail);
                   }
                 });
-           // Find devices based on decoded token
-           Device.find({ userEmail : decodedToken.email}, function(err, devices) {
-              if (!err) {
-                for (device of devices) {
-                  accountInfo['devices'].push({ deviceId: device.deviceId, apikey: device.apikey });
-                  /*for(BPMread of device.BPMreadings){
-                    accountInfo["BPMResults"].push(BPMread);
-                    console.log(BPMread);
-                    console.log("bmread loop");
+                // Find devices based on decoded token
+                Device.find({ userEmail : decodedToken.email}, function(err, devices) {
+                  if (!err) {
+                    for (device of devices) {
+                      accountInfo['devices'].push({ deviceId: device.deviceId, apikey: device.apikey });
+                    }
                   }
-                  for(OXread of device.O2readings){
-                    accountInfo["OXResults"].push(OXread);
-                    console.log(OXread);
-                    console.log("OXread loop");
-                  }*/
-                  //accountInfo["BPMResults"] = accountInfo["BPMResults"].concat(device.BPMreadings);
-                  //accountInfo["OXResults"] = accountInfo["OXResults"].concat(device.O2readings);
-                  //accountInfo["timestamps"] = accountInfo["timestamps"].concat(device.timestamps);
-                  //console.log(device.BPMreadings[0]+"BPMreading sarray exists");
-                  //console.log(device.O2readings[0]+"O2readings array exists");
-                }
-             }
-             //console.log(accountInfo["BPMResults"][0]+"before sending the response");
-             res.status(200).json(accountInfo);
-           });
+                //res.status(200).json(accountInfo);
+                });
+              res.status(200).json(accountInfo);
          }
        });
     }
